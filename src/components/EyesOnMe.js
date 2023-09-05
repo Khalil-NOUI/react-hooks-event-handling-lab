ab08dddfd8adeeddc6aa9003d744a86c0b021f46
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-09-05 03:03:44 +0100

    Completed assignments

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function heandleFocus () {
+    console.log("Good!")
+}
+
+function handleBlur () {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe() {
+    return(
+        <button onFocus={heandleFocus} onBlur={handleBlur}> Eyes on me </button>
+    )
+}
+
+export default EyesOnMe;
